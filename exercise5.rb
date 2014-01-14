4c2cc917515a98660b33db8d97b5f208562fbe59
Celia Dang <dang.celia@gmail.com>
2014-01-14 17:18:32 -0500

    deleted other codes

--- exercise5.rb
+++ exercise5.rb
@@ -1,20 +1,3 @@
-# f = 32
-# def temperature(c)
-# 	c = (f - 32) * 5/9
-# end
-
-# print temperature
-
-word = "Hello"
-name = "Celia"
-
-def printing_words(text)
-	puts text
-end
-
-printing_words(word)
-printing_words(name)
-
 puts "What is the current temperature in LA?"
 f = gets.chomp.to_i
 c = (f - 32) * 5/9.to_i
@@ -22,7 +5,6 @@ c = (f - 32) * 5/9.to_i
 def temperature(celsius)
 	puts celsius
 end
-
 
 puts "It is Celsius" 
 temperature(c)
