7344275bc754fad98647d0d09955e36dd741ab43
Celia Dang <dang.celia@gmail.com>
2014-01-15 11:56:25 -0500

    sorted alphabetically

--- exercise6.rb
+++ exercise6.rb
@@ -6,6 +6,19 @@ grocery_list << "rice"
 
 grocery_list.each {|x| puts "* #{x}"}
 
+puts grocery_list.length
+
+if grocery_list.include?("bananas")
+	puts "You need to pick up bananas"
+else
+	puts "You don't need to pick up bananas today"
+end
+
+puts grocery_list[1]
+
+grocery_list.sort
+
+grocery_list.each {|x| puts "* #{x}"}
 
 
 
